a155e375130a990975d87c53f421c59b77dd7a99
Chisaka <jjs-0004@outlook.jp>
2014-05-29 10:51:37 +0900

    2nd commit

---
+++ WebContent/delete.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <link href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css"
+    rel="stylesheet">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>予定削除画面</title>
+    </head>
+    <body>
+        <h1>予定を削除</h1>
+
+        <button type="button" onClick="location.href='Delete'">削除</button>
+    </body>
+</html>
--- src/jp/co/event/Delete.java
+++ src/jp/co/event/Delete.java
@@ -1,12 +1,18 @@
 package jp.co.event;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class Delete
@@ -15,18 +21,32 @@ import javax.servlet.http.HttpServletResponse;
 public class Delete extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+    @Resource(name = "jdbc/crud")
+    private DataSource ds;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
     public Delete() {
         super();
-        // TODO Auto-generated constructor stub
+
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	       RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/delete.jsp");
+	        dispatcher.forward(request, response);
+
+	        try (Connection con = ds.getConnection();
+	                PreparedStatement ps = con
+	                        .prepareStatement("DELETE FROM SCHEDULELIST ")) {
+	            ps.setString(1, "%" +  + "%");
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+
 
 	}
 
